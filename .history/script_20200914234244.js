c6aac12ae5bd0b837ced130c05036d7542b75bcd
Rafa Virginio <rafael5845@gmail.com>
2020-12-30 22:26:51 -0300

    Update Version

--- .history/script_20200914234244.js
+++
@@ -1,7 +0,0 @@
-let GRvalidator = {
-    handleSubmit: (event) => {
-        event.event.preventDefault();
-    }
-}
-let form = document.querySelector('GRvalidator')
-form.addEventListener('submit', GRvalidator.handleSubmit);
